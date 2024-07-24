1286a7e3a139af6e7bb7879deb4b9668a53c6590
Begaiym Adylbek <mrmldsky27@gmail.com>
2024-07-24 21:27:10 +0200

    Adjust to heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
-app.listen(3000, () => {
-    console.log('App is running on http://localhost:3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App is running on http://localhost:${port}`)
 })
